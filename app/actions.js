26f6f5b4e8589632951ca2c411a62ca2546f6db6
Hannes Wallnöfer <hannesw@gmail.com>
2010-04-02 00:06:57 +0200

    Remove old code

--- app/actions.js
+++ app/actions.js
@@ -1,16 +1,5 @@
 include('ringo/webapp/response');
 
-module.shared = true;
-
-var Bayeux = org.cometd.Bayeux;
-var client;
-var channel;
-var listener = new org.cometd.MessageListener.Asynchronous({
-    deliver: function(from, to, message) {
-        print("got message: " + message);
-    }
-});
-
 exports.index = function index(req) {
     return skinResponse('skins/chat.html', {});
 };
