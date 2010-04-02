479afbcea1b540a04ffc4a16beb8e662037fb59f
Hannes Wallnöfer <hannesw@gmail.com>
2010-04-02 10:37:32 +0200

    Improve skin integration

--- app/actions.js
+++ app/actions.js
@@ -1,6 +1,9 @@
 include('ringo/webapp/response');
 
 exports.index = function index(req) {
-    return skinResponse('skins/chat.html', {});
+    return skinResponse('skins/chat.html', {
+        title: "CometD Chat (RingoJS version)",
+        contextPath: req.rootPath.slice(1)
+    });
 };
 
