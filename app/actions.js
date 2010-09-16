6c74926b339864b648ac428f470dec44b555c28f
Hannes Wallnöfer <hannesw@gmail.com>
2010-09-16 11:34:39 +0200

    Update to current response module

--- app/actions.js
+++ app/actions.js
@@ -1,7 +1,7 @@
-include('ringo/webapp/response');
+var {Response} = require('ringo/webapp/response');
 
 exports.index = function index(req) {
-    return skinResponse('skins/chat.html', {
+    return Response.skin('skins/chat.html', {
         title: "CometD Chat (RingoJS version)",
         contextPath: req.rootPath.slice(1)
     });
